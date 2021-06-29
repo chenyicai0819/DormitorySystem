429429bb38d7c26d3de1533d552869820c723b15
chenyicai0819 <2495842975@qq.com>
2021-06-29 22:42:21 +0800

    修改部署失败问题

---
+++ src/main/java/cn/edu/guet/controller/UserController.java
@@ -0,0 +1,4 @@
+package cn.edu.guet.controller;
+
+public class UserController {
+}
--- src/main/java/cn/edu/guet/util/DBConnection.java
+++ src/main/java/cn/edu/guet/util/DBConnection.java
@@ -15,11 +15,11 @@ public class DBConnection {
 	static{
 		try {
 			Properties properties = new Properties();
-			// 读取 jdbc.properties属性配置文件
+
 			InputStream resourceAsStream = DBConnection.class.getClassLoader().getResourceAsStream("db.properties");
-			// 从流中加载数据
+
 			properties.load(resourceAsStream);
-			// 创建数据库连接池
+
 			druidDataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -32,7 +32,7 @@ public class DBConnection {
 			if(null == conn){
 				conn = druidDataSource.getConnection();
 				connectionThreadLocal.set(conn);
-				conn.setAutoCommit(false);//设置为手动提交
+				conn.setAutoCommit(false);
 			}
 		} catch (SQLException throwables) {
 			throwables.printStackTrace();
@@ -41,12 +41,10 @@ public class DBConnection {
 	}
 
 
-	/**
-	 * 手动提交事务
-	 */
+
 	public static void commitAndClose(){
 		Connection conn = connectionThreadLocal.get();
-		if(conn != null){  //说明之前连接过数据库
+		if(conn != null){
 			try {
 				conn.commit();
 			} catch (SQLException e) {
@@ -55,25 +53,23 @@ public class DBConnection {
 				close(conn);
 			}
 		}
-		// 一定要执行 remove 操作，防止内存泄漏，否则就会出错。（因为 Tomcat 服务器底层使用了线程池技术）
+
 		connectionThreadLocal.remove();
 	}
 
-	/**
-	 * 回滚
-	 */
+
 	public static void rollbackAndClose(){
 		Connection conn = connectionThreadLocal.get();
 		if(conn != null){
 			try {
-				conn.rollback(); //回滚事务
+				conn.rollback();
 			} catch (SQLException throwables) {
 				throwables.printStackTrace();
 			}finally {
 				close(conn);
 			}
 		}
-		// 一定要执行 remove 操作，防止内存泄漏，否则就会出错。（因为 Tomcat 服务器底层使用了线程池技术）
+
 		connectionThreadLocal.remove();
 	}
 
